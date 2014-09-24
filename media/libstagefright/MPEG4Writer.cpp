9ef320cc4aac3ecd7218a7913244524608617ff6
Paras Nagda <pnagda@codeaurora.org>
2014-09-24 15:43:48 +0530

    libstagefright: Fix MPEG4Writer crash when internal memory is almost full.
     - Camcoder recording crashes when internal memory is almost full.
     - crash happens if there is no space to write even first video frame.
       as soon as first video frame comes file limit exceeded information
       is given back to recorder and timestamp of video is not updated.
       since video timestamp is not proper crash is seen
       when comparing it with audio timestamp.
     - check is added while doing comparision of timestamps and
       reporting error in track stop() so recorded file is not saved to
       memory.
    
    Change-Id: I69e8bad62ab03ce01de5cbdb13f8ea841b787fcb

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -1885,6 +1885,10 @@ status_t MPEG4Writer::Track::stop() {
     status_t err = static_cast<status_t>(reinterpret_cast<uintptr_t>(dummy));
 
     ALOGD("%s track stopped", mIsAudio? "Audio": "Video");
+    if (mOwner->exceedsFileSizeLimit() && mStszTableEntries->count() == 0) {
+        ALOGE(" Filesize limit exceeded and zero samples written ");
+        return ERROR_END_OF_STREAM;
+    }
     return err;
 }
 
@@ -3090,7 +3094,7 @@ void MPEG4Writer::Track::writePaspBox() {
 int32_t MPEG4Writer::Track::getStartTimeOffsetScaledTime() const {
     int64_t trackStartTimeOffsetUs = 0;
     int64_t moovStartTimeUs = mOwner->getStartTimestampUs();
-    if (mStartTimestampUs != moovStartTimeUs) {
+    if (mStartTimestampUs != moovStartTimeUs && mStszTableEntries->count() != 0) {
         CHECK_GT(mStartTimestampUs, moovStartTimeUs);
         trackStartTimeOffsetUs = mStartTimestampUs - moovStartTimeUs;
     }
