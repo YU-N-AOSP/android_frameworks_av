a448c0d4bdc680ff47395aa5c8d72fc8f2285f99
Yamit Mehta <ymehta@codeaurora.org>
2015-08-26 21:00:40 +0530

    audio: Audio resampler support for 192Khz playback
    Add support for QTI audio resampler
    
    audio: Audio resampler support for 192Khz playback
    Change-Id: Ia8f24a0874ebf6e16ef7bd1f2759a14f47149875
    
    audio: Add audio mixing support for qti resampler
    Change-Id: Ib657aa12b2a72323564148c302ff8891e1bb7433
    
    AudioMixer: Extend use of QTI resampler for 44.1Khz sampling rate
    Change-Id: I2a819dbc9f1e3e280cb4fa79328e331883a3e981
    
    AudioMixer: fill 0s at right place when no more buffers available
    Change-Id: I50504c5a02eb0c69abfc9b047792b0f6f85b9ce8
    
    audioflinger: add channel count check to use QTI resampler
    Change-Id: I8f76dd82b72a0dd8b77343e77e0d0545e1be2114
    
    Change-Id: Ia8f24a0874ebf6e16ef7bd1f2759a14f47149875

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -60,6 +60,14 @@ LOCAL_STATIC_LIBRARIES := \
     libcpustats \
     libmedia_helper
 
+#QTI Resampler
+ifeq ($(call is-vendor-board-platform,QCOM), true)
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER)), true)
+LOCAL_CFLAGS += -DQTI_RESAMPLER
+endif
+endif
+#QTI Resampler
+
 LOCAL_MODULE:= libaudioflinger
 LOCAL_32_BIT_ONLY := true
 
@@ -123,7 +131,19 @@ LOCAL_C_INCLUDES := \
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libdl \
-    liblog
+    liblog \
+    libaudioutils
+
+#QTI Resampler
+ifeq ($(call is-vendor-board-platform,QCOM), true)
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER)), true)
+LOCAL_SRC_FILES_$(TARGET_2ND_ARCH) += AudioResamplerQTI.cpp.arm
+LOCAL_C_INCLUDES_$(TARGET_2ND_ARCH) += $(TARGET_OUT_HEADERS)/mm-audio/audio-src
+LOCAL_SHARED_LIBRARIES_$(TARGET_2ND_ARCH) += libqct_resampler
+LOCAL_CFLAGS_$(TARGET_2ND_ARCH) += -DQTI_RESAMPLER
+endif
+endif
+#QTI Resampler
 
 LOCAL_MODULE := libaudioresampler
 
