87a693097fb30add2601ca89cf187c8cba374a3b
Deva Ramasubramanian <dramasub@codeaurora.org>
2015-09-17 12:48:59 -0700

    libstagefright: Allow for CameraSourceTimeLapse to be extended
    Add in hooks to route CameraSourceTimeLapse creation to AVFactory so
    that we can add our customizations.
    
    Change-Id: I309a247ba6e3c5626cfba096677a6dbfeae543f3

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1450,13 +1450,13 @@ status_t StagefrightRecorder::setupCameraSource(
             return BAD_VALUE;
         }
 
-        mCameraSourceTimeLapse = CameraSourceTimeLapse::CreateFromCamera(
+        mCameraSourceTimeLapse = AVFactory::get()->CreateCameraSourceTimeLapseFromCamera(
                 mCamera, mCameraProxy, mCameraId, mClientName, mClientUid,
                 videoSize, mFrameRate, mPreviewSurface,
                 mTimeBetweenCaptureUs);
         *cameraSource = mCameraSourceTimeLapse;
     } else {
-        *cameraSource = AVFactory::get()->CreateFromCamera(
+        *cameraSource = AVFactory::get()->CreateCameraSourceFromCamera(
                 mCamera, mCameraProxy, mCameraId, mClientName, mClientUid,
                 videoSize, mFrameRate,
                 mPreviewSurface);
