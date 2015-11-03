f67b638b179bed5a6810278fe245cec749b06569
Gao Jie <jzgao@dolby.com>
2015-11-03 20:43:59 +0800

    Merge dlb/android-6.0.0_r1/dax2
    Change-Id: I93d84769e5f3906178e1ee547ad9ccf83aaf1e5f
    (cherry picked from commit d6bc3bceac2f0b0882307e12c7ebbcebe6cace3b)

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2014-2015 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #include <inttypes.h>
@@ -32,6 +51,10 @@
 #include <media/stagefright/MediaSource.h>
 #include <media/stagefright/MetaData.h>
 #include <media/stagefright/Utils.h>
+
+#ifdef DOLBY_UDC_VIRTUALIZE_AUDIO
+#include "DolbyAudioPlayerExtImpl.h"
+#endif // DOLBY_END
 
 namespace android {
 
@@ -251,6 +274,9 @@ status_t AudioPlayer::start(bool sourceAlreadyStarted) {
 
     mStarted = true;
     mPlaying = true;
+#ifdef DOLBY_UDC_VIRTUALIZE_AUDIO
+    setDolbyProcessedAudio(format);
+#endif // DOLBY_END
 
     return OK;
 }
